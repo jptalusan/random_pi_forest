b88c124b62c68aa27ca643dc0c97055560983b85
JP Talusan <jptalusan@gmail.com>
2018-01-04 13:16:47 +0900

    Fix mesh script

--- mesh/manscript.sh
+++ mesh/manscript.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+setupMesh() {
+    echo "Setup"
+}
+
 out=$(sudo batctl o | wc -l)
 ifBatExists=$(ip a show bat0 up | wc -l)
+echo "$out"
+echo "$out"
 echo "$ifBatExists"
-if [ "$ifBatExists" -gt 1 ] && [ "$out" -gt 2 ]; then
+echo "$ifBatExists"
+echo "$ifBatExists"
+if [[  "$ifBatExists" > 1  &&  "$out" > 2 ]]; then
     echo "Already setup batman!"
 else
     echo "starting batman"
@@ -36,6 +45,6 @@ else
     echo "Sleeping for 5 seconds"
     sleep 5s
 
-    sudo ifconfig bat0 172.27.0.3/16
+    sudo ifconfig bat0 172.27.0.1/16
     echo "Done!!!"
 fi
